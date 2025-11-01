94ca8ef6d70383f155ece39c9a8ae0d777388f1a
Leo Rocha <jaguarete.leo@gmail.com>
2025-11-01 00:03:16 -0300

    add command handler to index.js

--- index.js
+++ index.js
@@ -1,18 +1,68 @@
-// Require necessary discordjs classes
-const { Client, Events, GatewayIntentBits } = require('discord.js');
-
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Events, GatewayIntentBits, Collection, BaseInteraction, ChatInputCommandInteraction, MessageFlags, CommandInteraction } = require('discord.js');
+const { log, error } = require('node:console');
 const { DISCORD_TOKEN } = process.env;
 
 // Create a new client instance 
 const client = new Client({ intents: GatewayIntentBits.Guilds })
 
+// Log in to discord with client token
+client.login(DISCORD_TOKEN);
+
 // When the client is ready, run this code (only once).
-// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
-// It makes some properties non-nullable.
 client.once(Events.ClientReady, (readyClient) => {
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
-// Log in to discord with client token
-client.login(DISCORD_TOKEN);
+client.commands = new Collection();
+
+const foldersPath = path.join(__dirname, 'commands');
+const commandsFolder = fs.readdirSync(foldersPath);
+
+for (const folder of commandsFolder) {
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('js'));
+
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    // Set a new item in the Collection with the key as the command name and the value as the exported module
+    if ('data' in command && 'execute' in command) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    }
+  }
+}
+
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+  const command = interaction.client.commands.get(interaction.commandName);
+
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (err) {
+    console.error(err);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: 'There was an error while executing this command!',
+        flags: MessageFlags.Ephemeral,
+      });
+    } else {
+      await interaction.reply({
+        content: 'There was an error while executing this command!',
+        flags: MessageFlags.Ephemeral,
+      });
+    }
+
+  }
+
+  log(interaction);
+});
 
