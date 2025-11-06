78cf84b668feb4c62f1abb54d0742a78721acd78
Leo Rocha <jaguarete.leo@gmail.com>
2025-11-06 18:50:52 -0300

    add utility commands and subcommands

---
+++ commands/utility/echo.js
@@ -0,0 +1,41 @@
+const { ChannelType, SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('echo')
+    .setDescription('Replies with your input!')
+    .addStringOption((option) =>
+      option
+        .setName('input')
+        .setDescription('The input to echo back')
+        .setRequired(true)
+        .setMaxLength(2_000)
+    )
+    .addChannelOption((option) =>
+      option
+        .setName('channel')
+        .setDescription('Which channel to echo into')
+        .addChannelTypes(ChannelType.GuildText)
+    ),
+
+  async execute(interaction) {
+    const input = interaction.options.getString('input', true);
+    const channel = interaction.options.getChannel('channel', false) || interaction.channel;
+
+    const message = {
+      content: input,
+    };
+
+    if (channel !== interaction.channel) {
+      await interaction.reply({
+        content: `Message sent to channel ${channel}`,
+        flags: MessageFlags.Ephemeral,
+      })
+
+      return await channel.send(message);
+    }
+
+    return await interaction.reply(message);
+  }
+}
+
---
+++ commands/utility/info.js
@@ -0,0 +1,49 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('info')
+    .setDescription('Info Command')
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName('user')
+        .setDescription('Info about the user')
+        .addUserOption((option) =>
+          option
+            .setName('target')
+            .setDescription('User to be info\'d')
+        )
+    )
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName('server')
+        .setDescription('Info about the server')
+    ),
+
+  async execute(interaction) {
+    const subCommand = interaction.options.getSubcommand(true);
+    switch (subCommand) {
+      case 'user':
+        const targetUser = interaction.options.getUser('target', false) || interaction.user;
+        const userMember = await interaction.guild.members.fetch(targetUser.id);
+        // interaction.user is the object representing the User who ran the command
+        // interaction.member is the GuildMember object, which represents the user in the specific guild
+        await interaction.reply(
+          `This user is \`${targetUser.username}\`, who joined <t:${Math.floor(userMember.joinedTimestamp / 1000)}:R>.`,
+        );
+        break;
+
+      case 'server':
+        // interaction.user is the object representing the User who ran the command
+        // interaction.member is the GuildMember object, which represents the user in the specific guild
+        await interaction.reply(
+          `This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`
+        )
+        break;
+
+      default:
+        break;
+    }
+  }
+}
+
