a8d115826d5860bc11db7ed9cab20fa8938daa4f
Leo Rocha <jaguarete.leo@gmail.com>
2025-11-09 13:43:49 -0300

    feat ping command now properly pings
    with Easter Egg

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,9 +1,18 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, MessageFlags } = require('discord.js');
 
 module.exports = {
-  cooldown: 5,
+  cooldown: 2,
   data: new SlashCommandBuilder().setName('ping').setDescription('Replies with Pong!'),
   async execute(interaction) {
-    await interaction.reply('**Pong!!!**');
+    const reply = await interaction.deferReply({ content: `Pinging...`, withResponse: true });
+    const latency = reply.resource.message.createdTimestamp - interaction.createdTimestamp;
+
+    await interaction.editReply(`**Pong!!!** with latency \`${latency} ms\``);
+
+    const random = Math.round(Math.random() * 100);
+
+    if (random > 90) {
+      await interaction.followUp({ content: 'No one will believe you', flags: MessageFlags.Ephemeral });
+    }
   },
 };
