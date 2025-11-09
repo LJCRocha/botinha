846edf4031f6cae87e42181854720be6e90e001a
Leo Rocha <jaguarete.leo@gmail.com>
2025-11-09 13:06:38 -0300

    add delete-commands file (run with caution)

---
+++ delete-commands.js
@@ -0,0 +1,16 @@
+const { REST, Routes } = require('discord.js');
+const { GUILD_ID, TOKEN, CLIENT_ID } = process.env;
+
+const rest = new REST().setToken(TOKEN);
+
+// for guild commands
+rest
+  .put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: [] })
+  .then(() => console.log('Successfully deleted all guild application commands.'))
+  .catch(console.error);
+
+// for global commands
+rest
+  .put(Routes.applicationCommands(CLIENT_ID), { body: [] })
+  .then(() => console.log('Successfully deleted all application commands.'))
+  .catch(console.error);
