cb48201f58d83d06d2ed298e1b773938c40e4c9f
Leo Rocha <jaguarete.leo@gmail.com>
2025-11-12 16:13:04 -0300

    add command.js as devonly template command

--- commands/new/command.js
+++ commands/new/command.js
@@ -1,10 +1,33 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder, } = require("discord.js");
 
 module.exports = {
+    devonly: true,
     data: new SlashCommandBuilder()
         .setName('command')
         .setDescription('Command\'s description'),
+
+    /**
+     * @param {import('discord.js').ChatInputCommandInteraction} interaction
+     */
     async execute(interaction) {
-        return;
+        const exampleEmbed = new EmbedBuilder()
+            .setColor(0x0099ff)
+            .setTitle('Some title')
+            .setURL('https://discord.js.org/')
+            .setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
+            .setDescription('Some description here')
+            .setThumbnail('https://i.imgur.com/AfFp7pu.png')
+            .addFields(
+                { name: 'Regular field title', value: 'Some value here' },
+                { name: '\u200B', value: '\u200B' },
+                { name: 'Inline field title', value: 'Some value here', inline: true },
+                { name: 'Inline field title', value: 'Some value here', inline: true },
+            )
+            .addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
+            .setImage('https://i.imgur.com/AfFp7pu.png')
+            .setTimestamp()
+            .setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
+
+        interaction.channel.send({ embeds: [exampleEmbed] });
     }
 }
