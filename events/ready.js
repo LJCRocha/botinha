5a51266e3dbd8d8191f65646172d67f3b3aa8ee4
Leo Rocha <jaguarete.leo@gmail.com>
2025-12-21 21:25:29 -0300

    add: SQL DB support

--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,11 @@ const { Events } = require('discord.js');
 module.exports = {
     name: Events.ClientReady,
     once: true,
+    /**
+    * @param {import('../structures/BotClient.js')} client
+    */
     execute(client) {
+        client.tags.sync({ force: true });
         console.log(`Ready! Logged in as ${client.user.tag}`);
     },
 };
